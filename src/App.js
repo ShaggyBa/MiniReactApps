948b379452d224b69df4509bf3dc2606d15f6589
ShaggyBa <71100001+ShaggyBa@users.noreply.github.com>
2023-04-30 22:18:58 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './index.scss';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+	let [currentNumber, setCurrentNumber] = useState(0)
+
+	return (
+		<div className="App">
+			<div>
+				<h2>Счетчик:</h2>
+				<h1>{currentNumber}</h1>
+				<button className="minus" onClick={() => setCurrentNumber(currentNumber -= 1)}>- Минус</button>
+				<button className="plus" onClick={() => setCurrentNumber(currentNumber += 1)}>Плюс +</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
