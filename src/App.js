196294aa412f49aa2887c9dd32c8b5e04798e99f
ShaggyBa <71100001+ShaggyBa@users.noreply.github.com>
2023-05-04 18:48:17 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
+import data from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
-    </div>
-  );
+	const rates = data
+
+	const [fromCurrency, setFromCurrency] = useState("RUB")
+	const [toCurrency, setToCurrency] = useState("USD")
+
+	const [fromValue, setFromValue] = useState(rates[toCurrency])
+	const [toValue, setToValue] = useState(1)
+
+
+	const onChangeFromValue = (value) => {
+		if (value >= 0) {
+			const result = rates[toCurrency] / rates[fromCurrency] * value
+			setFromValue(value)
+			setToValue(result)
+
+		}
+	}
+
+	const onChangeToValue = (value) => {
+		if (value >= 0) {
+			const result = rates[fromCurrency] / rates[toCurrency] * value
+			setToValue(value)
+			setFromValue(result)
+
+		}
+	}
+
+	useEffect(() => {
+		onChangeFromValue(fromValue)
+	}, [fromCurrency, fromValue])
+
+	useEffect(() => {
+		onChangeToValue(toValue)
+	}, [toCurrency, toValue])
+
+	return (
+		<div className="App">
+			<Block value={fromValue}
+				currency={fromCurrency}
+				onChangeCurrency={setFromCurrency}
+				onChangeValue={onChangeFromValue} />
+			<Block value={toValue}
+				currency={toCurrency}
+				onChangeCurrency={setToCurrency}
+				onChangeValue={onChangeToValue}
+			/>
+		</div>
+	);
 }
 
 export default App;
--- src/Block.jsx
+++ src/Block.jsx
@@ -1,30 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data';
 
-const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP'];
+const rates = ['RUB', 'USD', 'EUR', 'GBP'];
+// const _data = Object.keys(data)
 
-export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => (
-  <div className="block">
-    <ul className="currencies">
-      {defaultCurrencies.map((cur) => (
-        <li
-          onClick={() => onChangeCurrency(cur)}
-          className={currency === cur ? 'active' : ''}
-          key={cur}>
-          {cur}
-        </li>
-      ))}
-      <li>
-        <svg height="50px" viewBox="0 0 50 50" width="50px">
-          <rect fill="none" height="50" width="50" />
-          <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
-        </svg>
-      </li>
-    </ul>
-    <input
-      onChange={(e) => onChangeValue(e.target.value)}
-      value={value}
-      type="number"
-      placeholder={0}
-    />
-  </div>
-);
+
+export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => {
+	// const [maxRates, setMaxRates] = useState(4)
+
+	// const [rates, setRates] = useState(getRates(maxRates))
+
+	// function getRates(maxRates) {
+	// 	const rates = []
+	// 	for (let i = 0; i < maxRates; i++) {
+	// 		rates.push(_data[i])
+	// 	}
+	// 	return rates
+	// }
+
+	// const onChangeMaxRates = () => {
+	// 	setMaxRates(_data.length)
+	// 	setRates(getRates(maxRates))
+	// }
+
+	return (
+
+		<div className="block">
+			<ul className="currencies">
+
+				{rates.map((cur) => (
+					<li
+						onClick={() => onChangeCurrency(cur)}
+						className={currency === cur ? 'active' : ''}
+						key={cur}>
+						{cur}
+					</li>
+				))}
+				<li
+				// onClick={onChangeMaxRates}
+				>
+					<svg height="50px" viewBox="0 0 50 50" width="50px">
+						<rect fill="none" height="50" width="50" />
+						<polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
+					</svg>
+				</li>
+			</ul>
+			<input
+				onChange={(e) => onChangeValue(Number(e.target.value))}
+				value={value}
+				type="number"
+				placeholder={0}
+				maxLength={8}
+			/>
+		</div>
+	)
+};
